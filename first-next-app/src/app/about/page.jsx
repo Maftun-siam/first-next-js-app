53b2286f66629671b265ab2f6ecc7b8fd95d25a1
Maftun-siam <maftunhasans@gmail.com>
2024-09-29 14:16:09 +0600

    dff

---
+++ first-next-app/src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import AboutsChild from '@/components/AboutsChild';
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h2>This is about page</h2>
+          
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ first-next-app/src/components/AboutsChild.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutsChild = () => {
+    return (
+        <div>
+            <h2>This is abouts child</h2>
+        </div>
+    );
+};
+
+export default AboutsChild;
